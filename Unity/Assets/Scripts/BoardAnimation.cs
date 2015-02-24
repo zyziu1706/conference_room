ab2c9ad1b47b0786c8c250161ea0a7969f9ab4ce
Kacper Zych <kaczych@student.agh.edu.pl>
2015-02-24 19:39:48 +0100

    First Board animation. Click "w" and "s" to move board.

---
+++ Unity/Assets/Scripts/BoardAnimation.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoardAnimation : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		if ( Input.GetKey(KeyCode.W)){
+			Debug.Log("Click");
+			animation.Play ("Board_FadeIN");
+
+		}else if (Input.GetKey(KeyCode.S)){
+			animation.Play ("Board_FadeOUT");
+
+		}
+	
+	}
+}
