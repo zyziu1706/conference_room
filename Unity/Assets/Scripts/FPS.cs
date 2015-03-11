04b6545e7e5b99a65f57465d50c02852a2175030
Michał Rybczyński <mryba1@gazeta.pl>
2015-03-11 21:08:01 +0100

    Poszerzenie GetIn.cs

--- Unity/Assets/Scripts/FPS.cs
+++ Unity/Assets/Scripts/FPS.cs
@@ -20,8 +20,8 @@
 	[AddComponentMenu("Camera-Control/Mouse Look")]
 public class FPS : MonoBehaviour {
 		
-		public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
-		public RotationAxes axes = RotationAxes.MouseXAndY;
+		//public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
+		//public RotationAxes axes = RotationAxes.MouseXAndY;
 		public float sensitivityX = 10F;
 		public float sensitivityY = 10F;
 		
@@ -32,49 +32,57 @@ public class FPS : MonoBehaviour {
 		public float maximumY = 50F;
 		
 		public int speed = 30;
-
+		public bool canMove = true;
 		float rotationY = 0F;
 		
 		void Update ()
 		{
+			//Check if moving is posiible
+			if (canMove == true) {
+				//Mouse camera rotation controll
+				transform.Rotate (0, Input.GetAxis ("Mouse X") * sensitivityX, 0);
 
-			//Mouse camera rotation controll
-			if (axes == RotationAxes.MouseXAndY)
-			{
-				float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
-				
-				rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-				rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
-				
-				transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
-			}
-			else if (axes == RotationAxes.MouseX)
-			{
-				transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
-			}
-			else
-			{
-				rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-				rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
-				
-				transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
-			}
+				/* TO MOŻE SIĘ PRZYDAĆ JAK BĘDZIEMY CHCIELI ROBIĆ SWOBODNY RUCH MYSZĄ
+				if (axes == RotationAxes.MouseXAndY)
+				{
+					float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+					
+					rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+					rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
+					
+					transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+				}
+				else if (axes == RotationAxes.MouseX)
+				{
+					transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
+				}
+				else
+				{
+					rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+					rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
+					
+					transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
+				}
+				*/
 
-			//Keyboard camera movement controll
-			if (Input.GetKey(KeyCode.W)){
-				transform.position += transform.forward * speed * Time.deltaTime; 
+				//Keyboard camera movement controll
+				if (Input.GetKey (KeyCode.W)) {
+						transform.position += transform.forward * speed * Time.deltaTime; 
 
-			}if (Input.GetKey(KeyCode.S)){
-				transform.position += transform.forward * -speed * Time.deltaTime; 
-				
-			}if (Input.GetKey(KeyCode.A)){
-				transform.position += transform.right * -speed* Time.deltaTime; 
-				
-			}if (Input.GetKey(KeyCode.D)){
-				transform.position += transform.right * speed * Time.deltaTime; 
-				
+				}
+				if (Input.GetKey (KeyCode.S)) {
+						transform.position += transform.forward * -speed * Time.deltaTime; 
+		
+				}
+				if (Input.GetKey (KeyCode.A)) {
+						transform.position += transform.right * -speed * Time.deltaTime; 
+		
+				}
+				if (Input.GetKey (KeyCode.D)) {
+						transform.position += transform.right * speed * Time.deltaTime; 
+		
+				}
 			}
-			
 
 		}
 
---
+++ Unity/Assets/Scripts/GetIn.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GetIn : MonoBehaviour {
+
+	public Text pressToEnter;
+
+	private bool getIn = false;
+	private string name;
+
+	void Awake(){
+		pressToEnter.text = "";
+	}
+
+	void Update(){
+		if (getIn == true && Input.GetKey(KeyCode.E)){
+			Camera.main.transform.position = new Vector3(-12, 11, 0);
+			//TODO
+			//Trzeba ustawić canMove na false w skrypcie FPS.cs żeby zablokować poruszanie
+		}
+	}
+
+	void OnTriggerEnter(Collider obj){ 
+		if (obj.tag == "MainCamera") {
+			getIn = true;
+
+			switch (tag)
+			{
+			case "UserScreenTrigger":
+				name = "User Screen";
+				break;
+			case "WhiteBoardTrigger":
+				name = "Whiteboard";
+				break;
+			case "PreziBoardTrigger":
+				name = "Prezi Board";
+				break;
+			}
+
+			pressToEnter.text = "Press E to enter " + name;
+		}
+	}
+	void OnTriggerExit(Collider obj){  
+			if (obj.tag == "MainCamera") {
+				getIn = false; 
+				pressToEnter.text = "";
+			}
+	}
+
+}
